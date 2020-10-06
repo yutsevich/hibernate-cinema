cc5a279546badec3961fd27b1c68842df0302bfe
Yutsevich Anastasia <yutsevicha@ gmail.com>
2020-10-06 20:26:16 +0300

    implemented CinemaHallDao and MovieSessionDao

---
+++ src/main/java/com/dev/cinema/dao/impl/MovieSessionDaoImpl.java
@@ -0,0 +1,59 @@
+package com.dev.cinema.dao.impl;
+
+import com.dev.cinema.dao.MovieSessionDao;
+import com.dev.cinema.exceptions.DataProcessingException;
+import com.dev.cinema.lib.Dao;
+import com.dev.cinema.model.MovieSession;
+import com.dev.cinema.util.HibernateUtil;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+@Dao
+public class MovieSessionDaoImpl implements MovieSessionDao {
+
+    @Override
+    public List<MovieSession> findAvailableSessions(Long movieId, LocalDate date) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<MovieSession> criteriaQuery = criteriaBuilder
+                    .createQuery(MovieSession.class);
+            Root<MovieSession> root = criteriaQuery.from(MovieSession.class);
+            Predicate checkMovie = criteriaBuilder.equal(root.get("movie"), movieId);
+            Predicate checkDate = criteriaBuilder.between(root.get("showTime"),
+                    date.atTime(LocalTime.MIN), date.atTime(LocalTime.MAX));
+            return session.createQuery(criteriaQuery.where(checkMovie, checkDate)).getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can not get all available movie sessions ", e);
+        }
+    }
+
+    @Override
+    public MovieSession add(MovieSession movieSession) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(movieSession);
+            transaction.commit();
+            return movieSession;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can not insert movie session hall entity "
+                    + movieSession.toString(), e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+}
