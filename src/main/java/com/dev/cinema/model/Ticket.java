0f4e1cff3554ac02fc4548465a458d1b8be0e91c
Yutsevich Anastasia <yutsevicha@ gmail.com>
2020-10-08 14:02:39 +0300

    implemented shopping cart and ticket dao layers, shopping cart service

--- src/main/java/com/dev/cinema/model/Ticket.java
+++ src/main/java/com/dev/cinema/model/Ticket.java
@@ -1,28 +1,26 @@
 package com.dev.cinema.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Entity
 @Data
-@ToString(exclude = "password")
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "users")
-public class User {
+@Table(name = "tickets")
+public class Ticket {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(unique = true)
-    private String email;
-    private String password;
-    private byte[] salt;
+    @OneToOne
+    private MovieSession movieSession;
+    @OneToOne
+    private User user;
 }
