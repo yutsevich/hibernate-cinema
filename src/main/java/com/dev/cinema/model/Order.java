fff5f21d332f2f828edc5e54b6b47062715dcc56
Yutsevich Anastasia <yutsevicha@ gmail.com>
2020-10-12 17:36:24 +0300

    changed relationship between order and user to "Many to One"

--- src/main/java/com/dev/cinema/model/Order.java
+++ src/main/java/com/dev/cinema/model/Order.java
@@ -3,13 +3,14 @@ package com.dev.cinema.model;
 import java.time.LocalDateTime;
 import java.util.List;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.MapsId;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -24,7 +25,7 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @OneToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @MapsId
     @JoinColumn(name = "order_id")
     private User user;
